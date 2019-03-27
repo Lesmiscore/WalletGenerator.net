26ee5a0573778ed3dacf2ca2877739cee936d05c
nao20010128nao <nao20010128@gmail.com>
2019-03-27 01:40:11 +0000

    いらなかった

--- tests/coinimage.js
+++ tests/coinimage.js
@@ -8,10 +8,10 @@ describe("coin image", function() {
   const names = janin.currencies.map(a => `${a.name}`.toLowerCase());
   for (let i of names) {
     it(`${i} has a valid logo`, function() {
-      fs.statSync(`logos/${i.toLowerCase()}.png`);
+      fs.statSync(`logos/${i}.png`);
     });
     it(`${i} has a valid wallet background`, function() {
-      fs.statSync(`wallets/${i.toLowerCase()}.png`);
+      fs.statSync(`wallets/${i}.png`);
     });
   }
 });
