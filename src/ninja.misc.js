915f3bc71063a363fd07ac9d2fd32f8e525a3190
nao20010128nao <nao20010128@gmail.com>
2019-09-30 02:26:58 +0000

    使われてなくて使用予定が無いコードを削除

--- src/ninja.misc.js
+++ src/ninja.misc.js
@@ -58,58 +58,6 @@ const getQueryString = function() {
   return result;
 };
 
-// use when passing an Array of Functions
-const runSerialized = function(functions, onComplete) {
-  onComplete = onComplete || function() {};
-
-  if (functions.length === 0) onComplete();
-  else {
-    // run the first function, and make it call this
-    // function when finished with the rest of the list
-    setTimeout(() => {
-      const f = functions.shift();
-      f(function() {
-        runSerialized(functions, onComplete);
-      });
-    }, 0);
-  }
-};
-
-const forSerialized = function(initial, max, whatToDo, onComplete) {
-  onComplete = onComplete || function() {};
-
-  if (initial === max) {
-    onComplete();
-  } else {
-    // same idea as runSerialized
-    setTimeout(() => {
-      whatToDo(initial, function() {
-        forSerialized(++initial, max, whatToDo, onComplete);
-      });
-    }, 0);
-  }
-};
-
-// use when passing an Object (dictionary) of Functions
-const foreachSerialized = function(collection, whatToDo, onComplete) {
-  const keys = [];
-  for (const name in collection) {
-    if ({}.hasOwnProperty.call(collection, name)) {
-      keys.push(name);
-    }
-  }
-  setTimeout(() => {
-    forSerialized(
-      0,
-      keys.length,
-      function(i, callback) {
-        whatToDo(keys[i], callback);
-      },
-      onComplete
-    );
-  }, 0);
-};
-
 const toggleFaqQuestion = function(elementId) {
   const answerDiv = document.getElementById(elementId);
   answerDiv.style.display = answerDiv.style.display === "block" ? "none" : "block";
@@ -120,8 +68,5 @@ module.exports = {
   envSecurityCheck,
   browserSecurityCheck,
   getQueryString,
-  runSerialized,
-  forSerialized,
-  foreachSerialized,
   toggleFaqQuestion
 };
