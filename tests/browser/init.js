125ff940e0d5dfb90887cc5a29ad75d1521d7565
nao20010128nao <nao20010128@gmail.com>
2020-07-16 01:38:57 +0000

    GitHub Actions
    とりあえずテストのみ

--- tests/browser/init.js
+++ tests/browser/init.js
@@ -1,7 +1,7 @@
 /* global mocha, before, beforeEach, afterEach, after */
 async function load() {
   await import("../../src/init.js");
-  const onload = (await import("../../src/ninja.misc")).onload;
+  const { onload } = await import("../../src/ninja.misc");
   const mochaDiv = document.createElement("div");
   mochaDiv.id = "mocha";
   document.body.appendChild(mochaDiv);
