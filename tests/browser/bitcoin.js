a70179fd0c94b50a9f336f82c74d49b4243057a3
nao20010128nao <nao20010128@gmail.com>
2020-08-20 01:33:09 +0000

    修正

--- tests/browser/bitcoin.js
+++ tests/browser/bitcoin.js
@@ -1,8 +1,6 @@
 /* global describe, it */
-const assert = require("assert");
-
-const bitcoinCurrency = require("../../src/janin.currency").currencies.find((curr) => curr.name === "Bitcoin");
-describe("bitcoin", function () {
+describe("bitcoin", async function () {
+  const bitcoinCurrency = (await import("../../src/janin.currency")).currencies.find((curr) => curr.name === "Bitcoin");
   ["L5JywexZwWuN9rM2Hs9zwudmwLBHNqqjcAh1xFhv6g413mufdXFG", "5KFBSucisZhx7mY3ocLLkaysWFZPirEF3N3yD5G7MRdqp5EGa93"].forEach((item, index) => {
     it(`can load ordinary WIF ${index}`, function () {
       bitcoinCurrency.decodePrivateKey(item);
